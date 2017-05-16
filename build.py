fbc48fac234901c37dd52dc043d852c6ffc69b0d
jay <jaymewada@live.com>
2017-05-16 14:34:34 +0530

    done

---
+++ build.py
@@ -0,0 +1,22 @@
+class Node(object):
+
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def traverse(rootnode):
+        thislevel = [rootnode]
+        while thislevel:
+            nextlevel = list()
+            for n in thislevel:
+                print n.value,
+                if n.left: nextlevel.append(n.left)
+                if n.right: nextlevel.append(n.right)
+            print
+            thislevel = nextlevel
+
+t = Node(1, Node(2, Node(4, Node(7))), Node(3, Node(5), Node(6)))
+
+result = Node(1,4)
+print (result)
